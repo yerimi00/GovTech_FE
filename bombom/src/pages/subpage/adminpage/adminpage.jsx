de564f2cd833e4dff659b38ac2e152bde4cfa630
nayeong lee <lny021102@gmail.com>
2024-08-21 05:54:47 +0900

    [ADD] admin mainpage 디자인 추가

--- bombom/src/pages/subpage/adminpage/adminpage.jsx
+++ bombom/src/pages/subpage/adminpage/adminpage.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import CustomColumn from '../../../components/container/CustomColumn';
 import CustomRow from '../../../components/container/CustomRow';
 import CustomFont from '../../../components/container/CustomFont';
+import WantCategory from '../../../components/container/WantCategory';
 
 const ContainerCenter = styled.div`
   display: flex;
@@ -21,39 +22,34 @@ const PageContainer = styled(ContainerCenter)`
   justify-content: center;
   gap: 4rem;
   position: relative;
-  background-color: white;
-`;
-
-const ModalBackground = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  z-index: 1000;
-  align-items: center;
-`;
-
-const Modal = styled.div`
-  background-color: white;
-  padding: 2rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  z-index: 1001;
+  background: linear-gradient(to bottom, #e5ddc9, white);
 `;
 
 const MenuButton = styled.button`
-background-color: #AFAFAF;
+background-color: ${(props) => props.backgroundColor || "#AFAFAF"};
 border: none;
+border-radius: 5rem;
 display: flex;
 align-items: center;
 justify-content: center;
 width: 100%;
 color: black;
 padding: 1rem;
+`;
+
+const MainDiv = styled.div`
+  background-color: ${(props) => props.backgroundColor || "#AFAFAF"};
+  border: none;
+  width: ${(props) => props.width || "100%"};
+  height: ${(props) => props.height || "auto"};
+  border-radius: ${(props) => props.borderRadius || "auto"};
+  min-height: 6rem;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: ${(props) => props.fontFamily || "Noto Sans KR"};
 `;
 
 const AdminPage = () => {
@@ -78,23 +74,39 @@ const AdminPage = () => {
     return (
         <ContainerCenter>
             <PageContainer>
-                <CustomColumn width='90%' alignItems='center' justifyContent='center' gap='2rem'>
+                <CustomColumn width='90%' alignItems='center' justifyContent='center'>
+                    <MainDiv backgroundColor="white" borderRadius="35px">
+                        <CustomRow width='100%' alignItems='center' justifyContent='flex-start'>
+                            <WantCategory width="40%" borderRadius="20px">
+                                관리자 화면
+                            </WantCategory>
+                        </CustomRow>
+                        <CustomColumn width='90%' alignItems='center' justifyContent='center' gap='2rem'>
+                            <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
+                                <CustomFont color='#6E5F3B' font='1.5rem'>돌보미 관리</CustomFont>
+                                <MenuButton backgroundColor='#FBDA8B' onClick={goDolbomOk}>
+                                    <CustomFont color='#6E5F3B' font='1rem'>돌보미 인증 수락 화면</CustomFont>
+                                </MenuButton>
+                                <MenuButton backgroundColor='#FBDA8B' onClick={goDolbomEduVideo}>
+                                    <CustomFont color='#6E5F3B' font='1rem'>교육 동영상 관리 화면</CustomFont>
+                                </MenuButton>
+                            </CustomColumn>
 
-                    <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
-                        <CustomFont color='black' font='1rem'>돌보미 관리</CustomFont>
-                        <MenuButton onClick={goDolbomOk}>돌보미 인증 수락 페이지</MenuButton>
-                        <MenuButton onClick={goDolbomEduVideo}>교육 동영상 관리 페이지</MenuButton>
-                    </CustomColumn>
+                            <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
+                                <CustomFont color='#5E694D' font='1.5rem'>보호자 관리</CustomFont>
+                                <MenuButton backgroundColor='#D6EFAE' onClick={goLowProtectorOk}>
+                                    <CustomFont color='#6E5F3B' font='1rem'>저소득층 인증 수락 화면</CustomFont>
+                                </MenuButton>
+                            </CustomColumn>
 
-                    <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
-                        <CustomFont color='black' font='1rem'>보호자 관리</CustomFont>
-                        <MenuButton onClick={goLowProtectorOk}>저소득층 인증 수락 페이지</MenuButton>
-                    </CustomColumn>
-
-                    <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
-                        <CustomFont color='black' font='1rem'>고객 관리</CustomFont>
-                        <MenuButton onClick={goNotice}>고객센터 페이지</MenuButton>
-                    </CustomColumn>
+                            <CustomColumn width='100%' alignItems='flex-start' justifyContent='center' gap='1rem'>
+                                <CustomFont color='#6E5F3B' font='1.5rem'>고객 관리</CustomFont>
+                                <MenuButton backgroundColor='#E5DDC9' onClick={goNotice}>
+                                    <CustomFont color='#6E5F3B' font='1rem'>고객 관리 화면</CustomFont>
+                                </MenuButton>
+                            </CustomColumn>
+                        </CustomColumn>
+                    </MainDiv>
                 </CustomColumn>
             </PageContainer>
         </ContainerCenter>
